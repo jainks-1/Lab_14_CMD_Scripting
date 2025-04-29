23e7424c1555d4797edecdb5c331a180ef7396b9
jainks-1 <jainks@mail.uc.edu>
2025-04-29 13:40:46 -0400

    code for submission

--- src/FileScan.java
+++ src/FileScan.java
@@ -1,14 +1,65 @@
 // Kaden Jain
-// Module 14
+// Module 14 lab
 
-public class Main {
+// import all the libraries using *
+import javax.swing.*;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption.*;
+
+import static java.nio.file.StandardOpenOption.CREATE;
+
+public class FileScan {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
+        JFileChooser chooser = new JFileChooser();
+        File selectedFile;
+        String line = ""; // line by line
+        String [] words; // array of words in line
+        int lineCount = 0;
+        int wordCount = 0;
+        int charCount = 0;
+        Path file = null;
 
-        for (int i = 1; i <= 5; i++) {
+        try {
+            File workingDirectory = new File(System.getProperty("user.dir"));
+            if(args.length == 0){
+                chooser.setCurrentDirectory(workingDirectory);
+                if(chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+                    selectedFile = chooser.getSelectedFile();
+                    file = selectedFile.toPath();
+                }
+                else {
+                    System.out.println("No file selected!!");
+                }
+            }
+            else{
+                file = new File(args[0]).toPath();
+            }
 
-            System.out.println("i = " + i);
+            InputStream in = new BufferedInputStream(Files.newInputStream(file, CREATE));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+            while (reader.ready())
+            {
+                line = reader.readLine();
+                words = line.split(" ");
+                lineCount++;
+                charCount += line.length();
+                wordCount += words.length;
+            }
+            reader.close();
+            System.out.println("Lines: " + lineCount);
+            System.out.println("Characters: " + charCount);
+            System.out.println("Words: " + wordCount);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        catch (Exception e) {
+            System.out.println("File not found");
+            e.printStackTrace();
         }
     }
 }
